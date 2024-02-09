8b749fb1226752db96e55fe79b9ad13f672625ff
exelimpichment <72119127+exelimpichment@users.noreply.github.com>
2024-02-10 00:55:02 +0100

    style: change project theme and buttons

--- app/sigInPage/components/SignWithEmailButton/SignWithEmailButton.tsx
+++ app/sigInPage/components/SignWithEmailButton/SignWithEmailButton.tsx
@@ -17,10 +17,10 @@ const SignWithEmailButton: React.FC<ISignWithEmailButton> = (
   return (
     <>
       <Button
+        className="w-full"
         disabled={props.isLoading}
         onClick={props.onClick}
         type="button"
-        className="h-9 w-full rounded-[6px] bg-zinc-900 text-zinc-50 hover:bg-slate-800 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-slate-50"
       >
         {props.isLoading && <LoadingSpinner size={18} />}
         <span className="ml-2">Sign In with Email</span>
